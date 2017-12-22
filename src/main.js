a6beba215880adef11dedf3d3cfaa64336b79495
witchkun <nyatta.hen@gmail.com>
2017-12-22 13:21:31 +0300

    using client libs

--- src/main.js
+++ src/main.js
@@ -1,70 +1,76 @@
 const express = require('express');
+const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 const url = require('url');
+const workspace = require('genesys-workspace-client-js');
+const authentication = require('genesys-authentication-client-js');
+
 let request = require('request');
 
 const app = express();
 const routes = express.Router();
 
-const apiUrl = "<apiUrl>";
-const apiKey = "<apiKey>";
+const apiUrl = "";
+const apiKey = "";
 const port = 8080;
 
-request = request.defaults({
-    jar: true,
-    headers: {
-        'x-api-key': apiKey
-    }
-});
+const currentUser = {};
 
-function isSuccess(response) {
-    return response.statusCode === 200;
-}
+const authClient = new authentication.ApiClient();
+authClient.basePath = `${apiUrl}/auth/v3`;
+authClient.defaultHeaders = {
+  'x-api-key': apiKey
+};
 
 routes.get('/user', (req, res) => {
-    request.get(`${apiUrl}/workspace/v3/current-session`, (err, response, body) => {
-        if(err) {
+    if(currentUser.authorization) {
+        const api = new authentication.AuthenticationApi(authClient);
+        api.getInfo(currentUser.authorization).then(data => {
+            res.send(data);
+        }).catch(err => {
             console.error(err);
-            res.send(err);
-        }
-        else if(isSuccess(response)) {
-            res.send(body);
-        }
-        else {
             res.redirect(401, '/login');
-        }        
-    });
+        });
+    }
+    else {
+        res.redirect(401, '/login');
+    }
 });
 
-routes.get('/login', (req, res) => {
-    const code = req.query.code;
+routes.post('/login', (req, res) => {
     const redirectUri = `${req.protocol}://${req.hostname}:${port}${req.path}`;
-    if(code) {
-        request.get(`${apiUrl}/workspace/v3/login?redirect_uri=${redirectUri}&code=${code}`, (err, response, body) => {
-            if(err) {
-                console.error(err);
-                res.send(err);
-            }
-            else if(isSuccess(response)) {
-                console.log(body);
-                res.redirect('/user');
-            }
-            else {
-                res.send(body);
-            }
-        });
-    }    
-    else {
-        res.redirect(`${apiUrl}/workspace/v3/login?redirect_uri=${redirectUri}`);
-    }
+    
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    const api = new authentication.AuthenticationApi(authClient);
+    api.signInWithHttpInfo(username, password).then(response => {
+        const data = response.body;
+        console.dir(response.request.headers);
+        console.dir(response.headers);
+        
+        const opts = {
+            
+        };
+        //api.retrieveToken('code', opts);
+        
+        res.send(data);
+        
+    }).catch(err => {
+        console.error(err);
+        res.send(err);
+    });
 });
 
 routes.get('/*', (req, res) => {
    res.redirect('/user'); 
 });
 
+app.use(express.static('public', {
+    extensions: ['html', 'htm']
+}));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
